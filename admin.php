22b955d5faf3b090b2baf211a4bcfdda3ffc8041
Esaken <kenesau4763@gmail.com>
2023-03-09 15:41:53 +0300

    added crud on admin

--- admin.php
+++ admin.php
@@ -21,6 +21,17 @@ include 'config.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Kamukunji  Shop</title>
     <link rel="stylesheet" href="css/foundation.css" />
+    <!-- Latest compiled and minified Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+	<!-- custom css -->
+	<style>
+	.m-r-1em{ margin-right:1em; }
+	.m-b-1em{ margin-bottom:1em; }
+	.m-l-1em{ margin-left:1em; }
+	.mt0{ margin-top:0; }
+	</style>
+  
     <script src="js/vendor/modernizr.js"></script>
   </head>
   <body>
@@ -57,42 +68,130 @@ include 'config.php';
     </nav>
 
 
-    <div class="row" style="margin-top:10px;">
-      <div class="large-12">
+    
         <h3>Hey Admin!</h3>
+       
+
+<!--  body-->
+ <!-- container -->
+ <div class="container">
+        <div class="page-header">
+            <h1>Read Products</h1>
+        </div>
+        <!-- PHP code to read records will be here -->
         <?php
-          $result = $mysqli->query("SELECT * from products order by id asc");
-          if($result) {
-            while($obj = $result->fetch_object()) {
-              echo '<div class="large-4 columns">';
-              echo '<p><h3>'.$obj->product_name.'</h3></p>';
-              echo '<img src="images/products/'.$obj->product_img_name.'"/>';
-              echo '<p><strong>Product Code</strong>: '.$obj->product_code.'</p>';
-              echo '<p><strong>Description</strong>: '.$obj->product_desc.'</p>';
-              echo '<p><strong>Units Available</strong>: '.$obj->qty.'</p>';
-              echo '<div class="large-6 columns" style="padding-left:0;">';
-              echo '<form method="post" name="update-quantity" action="admin-update.php">';
-              echo '<p><strong>New Qty</strong>:</p>';
-              echo '</div>';
-              echo '<div class="large-6 columns">';
-              echo '<input type="number" name="quantity[]"/>';
+// include database connection
+include 'config.php';
 
-              echo '</div>';
-              echo '</div>';
-            }
-          }
-        ?>
+// PAGINATION VARIABLES
+// page is the current page, if there's nothing set, default is page 1
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+// set records or rows of data per page
+$records_per_page = 5;
+// calculate for the query LIMIT clause
+$from_record_num = ($records_per_page * $page) - $records_per_page;
+
+// delete message prompt will be here
+$action = isset($_GET['action']) ? $_GET['action'] : "";
+// if it was redirected from delete.php
+if($action=='deleted'){
+	echo "<div class='alert alert-success'>Record was deleted.</div>";
+}
 
 
+// select all data
+                // $query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
+                // $stmt = $con->prepare($query);
+                // $stmt->execute();
 
-      </div>
-    </div>
+        //pagination
+    // select data for current page
+$query = "SELECT id, name, description, price FROM products ORDER BY id DESC
+LIMIT :from_record_num, :records_per_page";
+$stmt = $con->prepare($query);
+$stmt->bindParam(":from_record_num", $from_record_num, PDO::PARAM_INT);
+$stmt->bindParam(":records_per_page", $records_per_page, PDO::PARAM_INT);
+$stmt->execute();
 
 
-    <div class="row" style="margin-top:10px;">
-      <div class="small-12">
-        <center><p><input style="clear:both;" type="submit" class="button" value="Update"></p></center>
-        </form>
+// this is how to get number of rows returned
+$num = $stmt->rowCount();
+// link to create record form
+echo "<a href='create.php' class='btn btn-primary m-b-1em'>Create New Product</a>";
+//check if more than 0 record found
+if($num>0){
+	// data from database will be here
+//start table
+echo "<table class='table table-hover table-responsive table-bordered'>";
+	//creating our table heading
+	echo "<tr>
+		<th>ID</th>
+		<th>Name</th>
+		<th>Description</th>
+		<th>Price</th>
+		<th>Action</th>
+	</tr>";
+	// table body will be here
+   // retrieve our table contents
+// fetch() is faster than fetchAll()
+// http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+	// extract row
+	// this will make $row['firstname'] to
+	// just $firstname only
+	extract($row);
+	// creating new table row per record
+	echo "<tr>
+		<td>{$id}</td>
+		<td>{$name}</td>
+		<td>{$description}</td>
+		<td>{$price}</td>
+		<td>";
+			// read one record
+			echo "<a href='read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+			// we will use this links on next part of this post
+			echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+			// we will use this links on next part of this post
+			echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
+		echo "</td>";
+	echo "</tr>";
+}
+
+
+// PAGINATION
+// count total number of rows
+$query = "SELECT COUNT(*) as total_rows FROM products";
+$stmt = $con->prepare($query);
+// execute query
+$stmt->execute();
+// get total rows
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$total_rows = $row['total_rows'];
+
+
+// paginate records
+$page_url="admin.php?";
+include_once "paging.php";
+
+// end table
+echo "</table>";
+
+}
+// if no records found
+else{
+	echo "<div class='alert alert-danger'>No records found.</div>";
+}
+?>
+    </div> <!-- end .container -->
+<!--  body-->
+
+
+
+     
+   
+
+
+    
         <footer style="margin-top:10px;">
            <p style="text-align:center; font-size:0.8em;">&copy; Kennedy Esau. All Rights Reserved.</p>
         </footer>
@@ -103,9 +202,23 @@ include 'config.php';
 
 
 
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
+    <!-- confirm delete record will be here -->
+<script type='text/javascript'>
+// confirm record deletion
+function delete_user( id ){
+	var answer = confirm('Are you sure?');
+	if (answer){
+		// if user clicked ok,
+		// pass the id to delete.php and execute the delete query
+		window.location = 'delete.php?id=' + id;
+	}
+}
+</script>
     <script>
       $(document).foundation();
     </script>
--- config.php
+++ config.php
@@ -5,4 +5,13 @@ $db_password = '';
 $db_name = 'k_shop';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+
+
+try {
+    $con = new PDO("mysql:host={$db_host};dbname={$db_name}", $db_username, $db_password);
+}
+// show error
+catch(PDOException $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
 ?>
